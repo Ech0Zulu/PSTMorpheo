c7805b095e7018182785a078829cfc6fea0bb2a7
Echo <guillet@et.esiea.fr>
2025-01-27 20:19:25 +0100

    Bad Surgeon Init

--- Assets/Scripts/ButtonSys.cs
+++ Assets/Scripts/ButtonSys.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using System.IO.Ports;
 using UnityEngine;
 
@@ -8,28 +9,34 @@ public class ButtonSys : MonoBehaviour
     private LightSys lightSystem; // Light System Reference
     private ArduinoSys arduinoSystem; // Arduino System Reference
 
+    [SerializeField]
+    private GameObject lampLight; // Lumière dans Unity
+
+
     public float activationThreshold = 1.07f; // Activation Threshold
 
     private bool isPressed = false; // Button Actual State
 
-    private bool toggle = false; // Lamp Actual State
+    public bool toggle = false; // Lamp Actual State
 
     private void Start()
     {
-        lightSystem = GetComponent<LightSys>();
-        arduinoSystem = GetComponent<ArduinoSys>();
+        //lightSystem = GetComponent<LightSys>();
+        //arduinoSystem = GetComponent<ArduinoSys>();
     }
     private void Update()
     {
         float y = button.transform.position.y;
+        Debug.Log("button y : " +  y);
 
         // Check if the button has been pressed
         if (y <= activationThreshold && !isPressed)
         {
             isPressed = true; // Lock in pressed mode
             toggle = !toggle; // Switch lamp state
-            lightSystem.SetLightState(toggle);
-            arduinoSystem.SendToArduino(toggle);
+            lampLight.SetActive(toggle); // Active ou désactive la lumière
+            //lightSystem.SetLightState(toggle);
+            //arduinoSystem.SendToArduino(toggle);
         }
         // Check if the button went to it's original position
         else if (y > activationThreshold && isPressed)
